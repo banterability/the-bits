50a00f34a246a36649598cfcc23745c53b15ef5e
Jeff Long <jeff@carbonmade.com>
2015-10-28 16:45:37 -0500

    Add extend tests

---
+++ test/modules/extend_test.js
@@ -0,0 +1,18 @@
+var assert = require('assertive');
+var extend = require('../../modules/extend');
+
+describe('extend', function(){
+  it('extends the destination object with the attributes of the source object', function(){
+    var destination = {a: 1};
+    var source = {b: 2}
+    var expected = {a: 1, b: 2};
+    assert.deepEqual(expected, extend(destination, source));
+  });
+
+  it('overwrites existing properties in the destination object', function(){
+    var destination = {a: 1, b: 2};
+    var source = {b: 5}
+    var expected = {a: 1, b: 5};
+    assert.deepEqual(expected, extend(destination, source));
+  });
+});
